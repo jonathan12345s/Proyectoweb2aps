940b935d7e0a8fcc96352523727a3a0b2fe51d99
DESKTOP-0K97VU7\JONATHAN <jjgarciaa@est.utn.cr>
2017-07-10 10:58:57 -0600

    Tickets

--- ProyectoWEB2/ProyectoWEB2/Controllers/Tickets.cs
+++ ProyectoWEB2/ProyectoWEB2/Controllers/Tickets.cs
@@ -7,5 +7,33 @@ namespace ProyectoWEB2.Controllers
 {
     public class Tickets
     {
+
+
+
+
+
+        public int id { get; set; }
+
+
+        public String Titulo { get; set; }
+
+
+        public String detalle { get; set; }
+
+
+        public String reportado { get; set; }
+
+        public String clienye { get; set; }
+
+        public String estado { get; set; }
+
+
+
+        public virtual Cliente Clientecontacto { get; set; }
+
+
+
+
+
     }
 }

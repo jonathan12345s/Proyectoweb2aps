2f77a675f2fae64905f171ecb405111f6f76d7c9
DESKTOP-0K97VU7\JONATHAN <jjgarciaa@est.utn.cr>
2017-07-10 09:20:38 -0600

    login

---
+++ ProyectoWEB2/ProyectoWEB2/Controllers/Reunion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoWEB2.Controllers
+{
+    public class Reunion
+    {
+
+
+
+
+        //        Título de la reunión
+        //Dia y hora
+        //Usuario asignado(Listado de Usuarios del sistema, debe seleccionar al menos uno)
+        //Es Virtual(checkbox)
+        //Cliente(opcional)
+
+        public int id { get; set; }
+        public DateTime diayhora { get; set; }
+        public String usuarios { get; set; }
+
+        public String Virtual { get; set; }
+
+        public String clientes { get; set; }
+
+        public string ClienteNombre { get; set; }
+        //public virtual  Clientecontacto { get; set; }
+
+
+
+
+
+
+    }
+}
---
+++ ProyectoWEB2/ProyectoWEB2/Controllers/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoWEB2.Controllers
+{
+    public class Usuario
+    {
+
+
+        [Required]
+        [Display(Name = "User Login")]
+
+
+        public int id { get; set; }
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+
+
+
+
+
+    }
+}

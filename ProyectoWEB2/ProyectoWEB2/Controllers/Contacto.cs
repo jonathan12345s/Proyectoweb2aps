20c0bcc4fb89c4e9e55fdc40d9992d4d38a50395
DESKTOP-0K97VU7\JONATHAN <jjgarciaa@est.utn.cr>
2017-07-09 22:40:31 -0600

    datos nuevos

--- ProyectoWEB2/ProyectoWEB2/Controllers/Contacto.cs
+++ ProyectoWEB2/ProyectoWEB2/Controllers/Contacto.cs
@@ -7,5 +7,29 @@ namespace ProyectoWEB2.Controllers
 {
     public class Contacto
     {
+
+
+
+
+
+        public int id { get; set; }
+
+
+
+        public String Nombre { get; set; }
+
+        public String Apellidos { get; set; }
+
+        public String correo { get; set; }
+
+        public string telefono { get; set; }
+
+        public string puesto { get; set; }
+
+
+        public string ClienteNombre { get; set; }
+        public virtual Cliente Clientecontacto { get; set; }
+
+
     }
 }

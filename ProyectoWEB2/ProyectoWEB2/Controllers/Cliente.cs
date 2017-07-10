a5d7c868dc2f5e8869e5b02f81c525b4e08ff338
DESKTOP-0K97VU7\JONATHAN <jjgarciaa@est.utn.cr>
2017-07-09 22:36:58 -0600

    start

---
+++ ProyectoWEB2/ProyectoWEB2/Controllers/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoWEB2.Controllers
+{
+    public class Cliente
+    {
+
+        public int id { get; set; }
+
+        public String nombre { get; set; }
+        public string cedulaJuridica { get; set; }
+
+        public String paginaWeb { get; set; }
+
+        public int numeroTelefono { get; set; }
+
+        public String Sector { get; set; }
+
+    }
+}
